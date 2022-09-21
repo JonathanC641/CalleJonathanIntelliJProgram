7adf15d09090665856c71df447166a2cb62cd52d
Jonathan Calle <jonathanc641@nycstudents.net>
2022-09-21 16:46:22 -0400

    Added some more strings and a variable

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String [] args) {
         System.out.println("Hello World");
+        System.out.println("Chicken wings are fire");
+        int x = 5 + 6;
+        System.out.println(x);
     }
 }
